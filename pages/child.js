73678d131f9663b7f68f72d24c2355ab431ee625
jobmac <manzomariano@hotmail.com>
2022-10-10 11:06:22 -0300

    beta45

--- pages/child.js
+++ pages/child.js
@@ -51,13 +51,15 @@ export default function child() {
                             </Link>
                             <h4 className="mt-2 text-lg font-medium text-gray-700 dark:text-gray-200"> Espejo Quincho</h4>
                             <p className="text-blue-500">$12000</p>
-
+                            <Link
+                                href="https://www.mercadolibre.com/jms/mla/lgz/msl/login/H4sIAAAAAAAEA22Q3UvDMBTF_5fA-tSuqx9sFops-MF080lREQlZetvGpk1Nblbr2P_u3UB8EfKSc8_53cvZMW1K1XIcOmAp2_iBhazTAgtjG65y0hpNklMIv19xsAgrGkCwjqW7A6OEfAEUOlAKoR2QSXiseKFNT9pxDWnKcfiiXCs072GzVUDTYyBkpSFjhdi5NI77vh83YKXIjVYbC2NpmrGwcfkdywpkbTzGdO5lY3LIOlFCIE2LxM62qgvoPmiRe6uzI3B0Oh-d3NATFpX02vzHpvF6NY-SZHaRTKZnk0kEroMPE-XKQWsifrcOPr1oUeGQJQGXzhbZGh9r-WzOIysL-zq9Wl771aJ5mc3lfb2E6vbpYZgF1N6hzYzwf3S2D6kqhxytkDVL0XoIWQ5Sqxao-bf3_Q-VAgtVnwEAAA/notregistered">
                             <button className="flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mx-1" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                                 </svg>
                                 <span  className="mx-1">Comprar</span>
                             </button>
+                            </Link>
                         </div>
 
                         <div className="flex flex-col items-center justify-center w-full max-w-lg mx-auto">
