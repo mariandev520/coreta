670ec47d441b79c9c184df2159b0e4b4f4b1ec84
jobmac <manzomariano@hotmail.com>
2022-10-15 15:32:03 -0300

    beta75

--- pages/header.js
+++ pages/header.js
@@ -3,7 +3,7 @@ import 'transition-style';
 import { Text,  Box, Heading, Link, LinkBox, LinkOverlay } from "@chakra-ui/react";
 import "animate.css";  
 import Head from 'next/head';
-
+import { motion } from "framer-motion"
 
 export default function Header() {
     return (
@@ -17,15 +17,16 @@ export default function Header() {
 <div   >
 
 <div >
-<div  className="animate__animated animate__fadeInUp  text-uppercase font-semibold ">
+<div  className="animate__animated animate__fadeInUp  text-uppercase ">
 
 <h1 className="text-6xl text-rigth font-bold p-7 mb-10 ">
 
 </h1>
 
 
-
 <div className=" text-center mx-auto m6">
+
+
 <Link href="/">
  <button
  
@@ -36,21 +37,30 @@ export default function Header() {
  </button>
  </Link>
  <Link href="quien">
+ 
  <button
    type="button"
    className="border border-gray-200 bg-gray-300 text-gray-600 rounded-lg shadow-lg shadow-gray-500/50  px-8 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-400 focus:outline-none focus:shadow-outline"
  >
    TALLER
  </button>
+
  </Link>
+ 
+    
+
  <Link href="child">
+
  <button
    type="button"
    className="border border-gray-200 bg-green-200 text-gray-600 rounded-lg shadow-lg shadow-gray-500/50  px-8 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-400 focus:outline-none focus:shadow-outline"
  >
+  
    TIENDA
  </button>
  </Link>
+
+ 
  </div>
 </div>
 
