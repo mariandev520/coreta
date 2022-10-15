f3d3191ea0ab30429b76fb0879104cd3a7a17b5d
mariandev520 <manzomariano@hotmail.com>
2022-10-15 18:18:02 -0300

    beta72

--- pages/producoC.js
+++ pages/producoC.js
@@ -44,7 +44,7 @@ export default function productesC() {
 
   <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
     <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-      <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:tracking-tight sm:text-3xl">Deco</h1>
+      <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:tracking-tight sm:text-3xl">Caracteristicas</h1>
     </div>
 
 
--- pages/productes.js
+++ pages/productes.js
@@ -42,7 +42,7 @@ export default function productes() {
 
   <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
     <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-      <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:tracking-tight sm:text-3xl">Deco</h1>
+      <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:tracking-tight sm:text-3xl">Caracteristicas</h1>
     </div>
 
 
@@ -175,7 +175,7 @@ export default function productes() {
           </fieldset>
         </div>
 
-        <button type="submit" className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add to bag</button>
+        <button type="submit" className="mt-10 w-full bg-gray-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Comprar</button>
       </form>
     </div>
 
@@ -185,31 +185,31 @@ export default function productes() {
         <h3 className="sr-only">Descripcion</h3>
 
         <div className="space-y-6">
-          <p className="text-base text-gray-900">The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: &quot;Black&quot;. Need to add an extra pop of color to your outfit? Our white tee has you covered.</p>
+          <p className="text-base text-gray-900"></p>
         </div>
       </div>
 
       <div className="mt-10">
-        <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+        <h3 className="text-sm font-medium text-gray-900">Medidas</h3>
 
         <div className="mt-4">
           <ul role="list" className="pl-4 list-disc text-sm space-y-2">
-            <li className="text-gray-400"><span className="text-gray-600">Hand cut and sewn locally</span></li>
+            <li className="text-gray-400"><span className="text-gray-600">Ancho: 40cm </span></li>
 
-            <li className="text-gray-400"><span className="text-gray-600">Dyed with our proprietary colors</span></li>
+            <li className="text-gray-400"><span className="text-gray-600">Alto: 60cm</span></li>
 
-            <li className="text-gray-400"><span className="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
+            <li className="text-gray-400"><span className="text-gray-600">Gama arena marron</span></li>
 
-            <li className="text-gray-400"><span className="text-gray-600">Ultra-soft 100% cotton</span></li>
+            <li className="text-gray-400"><span className="text-gray-600">Cabañas o quinchos</span></li>
           </ul>
         </div>
       </div>
 
       <div className="mt-10">
-        <h2 className="text-sm font-medium text-gray-900">Details</h2>
+        <h2 className="text-sm font-medium text-gray-900">Detalles</h2>
 
         <div className="mt-4 space-y-6">
-          <p className="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
+          <p className="text-sm text-gray-600">Espejo pensado para espacios de reuniones rodeados de madera hogar a leña o quinchos</p>
         </div>
       </div>
     </div>
